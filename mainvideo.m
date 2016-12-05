8421d118e0d76b73325aec4a5e60397681555139
bradrichardson <brad.richardson@domo.com>
2016-12-05 16:39:05 -0700

    Count fire frames, output fire only video

--- mainvideo.m
+++ mainvideo.m
@@ -11,22 +11,56 @@ largeVideos = [string('forestfire.mp4'), string('firetruck.mp4'), string('helmet
 videos = smallVideos;
 
 for i = 1:size(videos, 2)
+    tic;
+
+    % Open videos for reading/writing
     file = videos(i);
     in = VideoReader(sprintf('./video/%s', file));
-    fprintf('Frames to read: %d\n', round(in.Duration*in.FrameRate));
+    frameCount = round(in.Duration*in.FrameRate);
+    fprintf('Frames to read: %d\n', frameCount);
+    outFireOnly = VideoWriter(sprintf('./out/fire-only-%s', file), 'MPEG-4');
+    outColored = VideoWriter(sprintf('./out/colored-%s', file), 'MPEG-4');
+    open(outFireOnly);
+    open(outColored);
 
-    out = VideoWriter(sprintf('./out/%s', file), 'MPEG-4');
-    open(out);
-    j = 0;
+    % Counters for previous frame fire detection and current frame index,
+    % overall frames with fire detected
+    previousFrames = zeros(100, 1);
+    previousFramesIdx = 1;
+    previousFramesFires = 0;
+    overallFireFrames = 0;
+    frameIdx = 1;
     while hasFrame(in)
         frame = readFrame(in);
-        [fireDetected, ~, ~] = FireDetection(frame, 40);
+
+        % Process and output results
+        tauValue = 40; % TODO - use previous frames' fires to calculate between 50-96
+        [fireDetected, fireOnlyIm, ~] = FireDetection(frame, tauValue);
+        previousFrames(previousFramesIdx) = nnz(fireDetected) > 0;
+        writeVideo(outFireOnly, fireOnlyIm);
         changedIm = MatchColorChange(frame, fireDetected, 0, 0, 255);
-        writeVideo(out, changedIm);
-        if mod(j,100) == 0
-            fprintf('%d frames read\n', j);
+        writeVideo(outColored, changedIm);
+
+        % Recalculate previous flames with fire for tau value,
+        % output every 100 frames to see progress and keep track of fire frames
+        previousFrameFires = nnz(previousFrames);
+        if mod(frameIdx,100) == 0
+            overallFireFrames = overallFireFrames + previousFrameFires;
+            fprintf('%d frames read (%d%% with fire)\n', frameIdx, previousFrameFires);
         end
-        j = j + 1;
+
+        % Increment counters
+        previousFramesIdx = previousFramesIdx + 1;
+        if previousFramesIdx > 100
+            previousFramesIdx = 1;
+        end
+        frameIdx = frameIdx + 1;
     end
-    close(out);
+
+    % Close all output files and print results
+    close(outFireOnly);
+    close(outColored);
+    fprintf('Total frames with fire: %d/%d (%.2f%%)\n', overallFireFrames,...
+        frameCount, 100.0*double(overallFireFrames)/double(frameCount));
+    toc;
 end
